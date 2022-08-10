c1e52730744dd7575ea83e83d7690e615241e411
Nizam Abdul <abdulnijamudeen@gmail.com>
2022-08-10 21:30:12 +0530

    Fixed imports and keywords

--- projects/ngx-style/src/public-api.ts
+++ projects/ngx-style/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/ngx-style.module';
 export * from './lib/directives/h/height/height.directive';
 export * from './lib/directives/m/margin/margin.directive';
 export * from './lib/directives/m/margin-block/margin-block.directive';
+export * from './lib/directives/m/margin-block-end/margin-block-end.directive';
 export * from './lib/directives/m/margin-block-start/margin-block-start.directive';
 export * from './lib/directives/m/margin-bottom/margin-bottom.directive';
 export * from './lib/directives/m/margin-inline/margin-inline.directive';
